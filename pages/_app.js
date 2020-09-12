3478c924c921e7fed81ccd4657aeaaa7ddfc7092
jihunhong <redgee49@gmail.com>
2020-09-12 22:51:53 +0900

    saga 설정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-/* eslint-disable */
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
+import withReduxSaga from 'next-redux-saga';
 
 import wrapper from '../store/configureStore';
 
@@ -21,4 +21,4 @@ App.propTypes = {
   Component: PropTypes.elementType.isRequired,
 };
 
-export default wrapper.withRedux(App);
+export default wrapper.withRedux((withReduxSaga(App)));
