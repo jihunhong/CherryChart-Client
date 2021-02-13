3596f18aa3c1b6e4b8aec963c9948a3533049945
jihunhong <redgee49@gmail.com>
2021-02-14 02:59:44 +0900

    SIDEBAR_WIDTH config로 분리

--- config.js
+++
@@ -1,2 +0,0 @@
-export const SIDEBAR_WIDTH = 250;
-// pixel
--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 export const baseURL = process.env.NODE_ENV === 'production' 
         ? 'https://cherrychart.com' 
         : 'http://localhost:8080';
+
+export const SIDEBAR_WIDTH = 250;
