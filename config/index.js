69077edc49f2de77b1a19333de31606cd6b9c2e3
jihunhong <redgee49@gmail.com>
2021-02-20 21:46:22 +0900

    baseURL 수정

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const baseURL = process.env.NODE_ENV === 'production' 
-        ? 'https://cherrychart.com' 
+        ? 'http://15.165.197.208' 
         : 'http://localhost:8080';
 
 export const SIDEBAR_WIDTH = 250;
