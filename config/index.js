893cf9b42303ae363b4449721d9650faebfe9157
jihunhong <redgee49@gmail.com>
2021-09-06 22:35:15 +0900

    baseURL 수정

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'https://cherrychart.com';
+export const baseURL = 'https://15.165.197.208';
 export const cdnURL = 'https://cdn.cherrychart.com';
 
 export const SIDEBAR_WIDTH = 250;
