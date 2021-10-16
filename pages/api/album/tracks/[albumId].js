be9fdfb3b81588b14ab6928fd42682a68735bd28
jihunhong <redgee49@gmail.com>
2021-10-17 01:00:38 +0900

    allowCors ilb 추가

--- pages/api/album/tracks/[albumId].js
+++ pages/api/album/tracks/[albumId].js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { baseURL } from '@config';
+import allowCors from '@lib/allowCors';
 
-export default async (req, res) => {
+export default allowCors(async (req, res) => {
   try {
     const response = await axios.get(`${baseURL}/api/album/tracks/${req.query.albumId}`);
     res.status(200).json(response.data);
   } catch (error) {
     res.send(error);
   }
-};
+});
--- pages/api/chart/[site]/index.js
+++ pages/api/chart/[site]/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 import { API_URL } from '@config';
+import allowCors from '@lib/allowCors';
 
-export default async (req, res) => {
+export default allowCors(async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/api/chart/${req.query.site}`);
     res.status(200).json(response.data);
   } catch (error) {
     res.send(error);
   }
-};
+});
