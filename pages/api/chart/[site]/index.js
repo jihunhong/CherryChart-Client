fb451910ab613b67c641b0fc5fdb229c85a86ef0
jihunhong <redgee49@gmail.com>
2021-11-06 01:53:19 +0900

    baseURL 수정

--- pages/api/chart/[site]/index.js
+++ pages/api/chart/[site]/index.js
@@ -1,10 +1,26 @@
-import axios from 'axios';
 import { API_URL } from '@config';
 import allowCors from '@lib/allowCors';
+import axios from 'axios';
 
 export default allowCors(async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/api/chart/${req.query.site}`);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
+    console.log(req.query.site);
     res.status(200).json(response.data);
   } catch (error) {
     res.send(error);
